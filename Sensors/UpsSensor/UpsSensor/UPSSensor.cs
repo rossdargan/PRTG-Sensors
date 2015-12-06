7358d784ddf44f90fd96163e2f7a7a3c741966d4
git@the-dargans.co.uk <kt596*^@RRq*&sM>
2015-12-06 13:59:46 +0000

    Fix a bug that caused the sensor host to crash

--- Sensors/UpsSensor/UpsSensor/UPSSensor.cs
+++ Sensors/UpsSensor/UpsSensor/UPSSensor.cs
@@ -10,6 +10,14 @@ namespace UpsSensor
 {
     public class UPSSensor : SensorHost.Shared.SensorBase
     {
+        private PowerChuteData _dataFactory;
+        public UPSSensor()
+        {
+            Console.WriteLine("Created instance of PowerChute");
+            _dataFactory = PowerChuteData.getInstance();
+
+        }
+
         public override IEnumerable<Result> Results()
         {
             return GetData();
@@ -17,23 +25,24 @@ namespace UpsSensor
 
         public List<Result> GetData()
         {
-            PowerChuteData dataFactory = PowerChuteData.getInstance();
-            var currentStatusData = dataFactory.GetCurrentStatusData();
-            
-
-            double energyUsage = currentStatusData.m_percentLoad / 100.0 * currentStatusData.m_config_active_power;          
-
             List<Result> results = new List<Result>();
-         
-            results.Add(new Result("Load Percent", currentStatusData.m_percentLoad)
-            {               
-                Unit = UnitTypes.Percent                
-            });
-            results.Add(new Result("Energy Usage", energyUsage)
+
+            try
             {
-                Unit = UnitTypes.Custom,
-                CustomUnit = "Watts"
-            });            
+                var currentStatusData = _dataFactory.GetCurrentStatusData();
+
+
+                double energyUsage = currentStatusData.m_percentLoad / 100.0 * currentStatusData.m_config_active_power;
+
+
+                results.Add(new Result("Load Percent", currentStatusData.m_percentLoad) { Unit = UnitTypes.Percent });
+                results.Add(new Result("Energy Usage", energyUsage) { Unit = UnitTypes.Custom, CustomUnit = "Watts" });
+                return results;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine($"Error getting data from UPS: {err}");
+            }
             return results;
         }
     }
